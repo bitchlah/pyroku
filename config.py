7214bdc9c78621ec56f3681b66f183448be15222
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-10-02 16:56:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -22,24 +22,24 @@ if not BLACKLIST_CHAT:
     BLACKLIST_CHAT = [-1001473548283]
 BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
 BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
-BOT_VER = "0.2.0@main"
+BOT_VER = "0.1.0@main"
 BRANCH = "main"
 CHANNEL = getenv("CHANNEL", "Lunatic0de")
 CMD_HANDLER = getenv("CMD_HANDLER", ".")
 DB_URL = getenv("DATABASE_URL", "")
 GIT_TOKEN = getenv(
     "GIT_TOKEN",
-    b64decode("Z2hwX1NEMWJ0T2phQ3Y3RFBXQmtHeUVCUTkyZWlKN09YVzFBVEY1RA==").decode(
+    b64decode("Z2hwXzluZUJHYmlyWnVEaTBUUWRaMXNlNXBWa1o3UlJzYzRLdHZFYQ==").decode(
         "utf-8"
     ),
 )
-GROUP = getenv("GROUP", "SharingUserbot")
+GROUP = getenv("GROUP", "ruangdiskusikami")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
 REPO_URL = getenv(
     "REPO_URL",
-    b64decode("aHR0cHM6Ly9naXRodWIuY29tL3Byb2plY3RtNG4vUHlyb01hbg==").decode("utf-8"),
+    b64decode("aHR0cHM6Ly9naXRodWIuY29tL2JpdGNobGFoL3B5cm9rdQ==").decode("utf-8"),
 )
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
