8c158e2def9e73d241e4a388ed58c4ef12fb45e6
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-10-03 16:06:46 +0700

    Update help.py

--- PunyaAlby/modules/help.py
+++ PunyaAlby/modules/help.py
@@ -1,75 +1,56 @@
-import asyncio
 
-from prettytable import PrettyTable
 from pyrogram import filters, Client
 from pyrogram.types import Message
 
 from PunyaAlby import CMD_HELP
-from PunyaAlby.helpers.utility import split_list
 
-heading = "──「 **{0}** 」──\n"
+@Client.on_message(filters.command("help"))
+async def helpmenu_handler(_, m: Message):
+    """ helpmenu handler for help plugin """
 
+    args = m.command if Client.long() > 1 else None
 
-@Client.on_message(filters.command("help", ".") & filters.me)
-async def module_help(client: Client, message: Message):
-    cmd = message.command
-
-    help_arg = ""
-    if len(cmd) > 1:
-        help_arg = " ".join(cmd[1:])
-    elif message.reply_to_message and len(cmd) == 1:
-        help_arg = message.reply_to_message.text
-    elif not message.reply_to_message and len(cmd) == 1:
-        all_commands = ""
-        all_commands += "Please specify which module you want help for!! \nUsage: `.help [module_name]`\n\n"
-
-        ac = PrettyTable()
-        ac.header = False
-        ac.title = "ALBY-Pyrobot Modules"
-        ac.align = "l"
-
-        for x in split_list(sorted(CMD_HELP.keys()), 2):
-            ac.add_row([x[0], x[1] if len(x) >= 2 else None])
-
-        await message.edit(f"```{str(ac)}```")
-
-    if help_arg:
-        if help_arg in CMD_HELP:
-            commands: dict = CMD_HELP[help_arg]
-            this_command = "**Help for**\n"
-            this_command += heading.format(str(help_arg)).upper()
-
-            for x in commands:
-                this_command += f"-> `{str(x)}`\n```{str(commands[x])}```\n"
-
-            await message.edit(this_command, parse_mode="markdown")
-        else:
-            await message.edit(
-                "`Please specify a valid module name.`", parse_mode="markdown"
+    try:
+        if not args:
+            await Client.send_edit(". . .", text_type=["mono"])
+            result = await Client.get_inline_bot_results(
+                Client.bot.username,
+                "#helpmenu"
             )
+            if result:
+                await m.delete()
+                info = await Client.send_inline_bot_result(
+                    m.chat.id,
+                    query_id=result.query_id,
+                    result_id=result.results[0].id,
+                    disable_notification=True,
+                )
 
-    await asyncio.sleep(10)
-    await message.delete()
+            else:
+                await Client.send_edit(
+                    "Please check your bots inline mode is on or not . . .",
+                    delme=3,
+                    text_type=["mono"]
+                )
+        elif args:
 
+            module_help = await Client.PluginData(args[1])
+            if not module_help:
+                await Client.send_edit(
+                    f"Invalid plugin name specified, use `{Client.Trigger()[0]}uplugs` to get list of plugins",
+                    delme=3
+                )
+            else:
+                await Client.send_edit(f"**MODULE:** {args[1]}\n\n" + "".join(module_help))
+        else:
+            await Client.send_edit("Try again later !", text_type=["mono"], delme=3)
+    except BotInlineDisabled:
+        await Client.toggle_inline()
+        await helpmenu_handler(_, m)
+    except Exception as e:
+        await Client.error(e)
 
-def add_command_help(module_name, commands):
-    """
-    Adds a modules help information.
-    :param module_name: name of the module
-    :param commands: list of lists, with command and description each.
-    """
-
-    # Key will be group name
-    # values will be dict of dicts of key command and value description
-
-    if module_name in CMD_HELP.keys():
-        command_dict = CMD_HELP[module_name]
-    else:
-        command_dict = {}
-
-    for x in commands:
-        for y in x:
-            if y is not x:
-                command_dict[x[0]] = x[1]
-
-    CMD_HELP[module_name] = command_dict
+@Client.on_message(filters.command("inline"))
+async def toggleinline_handler(_, m: Message):
+    """ toggleinline handler for help plugin """
+    return await Client.toggle_inline()
