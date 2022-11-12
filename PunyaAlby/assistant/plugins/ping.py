c21cfde6abe7298dda311f1705e512ddb4a51aab
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-11-12 23:39:33 +0700

    Update ping.py

--- PunyaAlby/assistant/plugins/ping.py
+++ PunyaAlby/assistant/plugins/ping.py
@@ -1,17 +1,21 @@
 from datetime import datetime
 
-from pyrogram import filters, Client
+from pyrogram import filters
 from pyrogram.types import Message
+from PunyaAlby.userbot.client import app
 
 
 
-@Client.bot.on_message(filters.command("ping"), group=-1)
+
+
+@app.bot.on_message(filters.command("ping"), group=-1)
 async def bot_ping(_, m: Message):
         start = datetime.now()
-        msg = await Client.bot.send_message(
+        msg = await app.bot.send_message(
             m.chat.id,
             "ping"
         )
         end = datetime.now()
         ms = (end - start).microseconds / 1000
-        await msg.edit(f"Pöng !\n`{ms}`\nUptime: `{Client.uptime()}`")
+        await msg.edit(f"Pöng !\n`{ms}`\nUptime: `{app.uptime()}`")
+
