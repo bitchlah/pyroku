7e108c192e2c24063b0de9b2eb6b00dffb25ac37
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-11-12 23:40:35 +0700

    Update start.py

--- PunyaAlby/assistant/plugins/start.py
+++ PunyaAlby/assistant/plugins/start.py
@@ -1,26 +1,28 @@
 import time
 
-from pyrogram import filters, Client
+from PunyaAlby.userbot.client import app
+
+from pyrogram import filters
 from pyrogram.types import Message
 
 
 
 
-@Client.bot.on_message(filters.command("start"), group=-1)
+@app.bot.on_message(filters.command("start"), group=-1)
 async def send_response(_, m: Message):
     await m.reply("How can i help you ?")
 
 
 
-@Client.bot.on_message(filters.new_chat_members & filters.group, group=1)
+@app.bot.on_message(filters.new_chat_members & filters.group, group=1)
 async def added_to_group_msg(_, m: Message):
     if m.new_chat_members[0].is_self:
         try:
-            await Client.bot.send_message(
+            await app.bot.send_message(
                 m.chat.id,
                 "Thank You for adding me in this group !\nUse /help to know my features."
             )
         except Exception as e:
-            await Client.error(m, e)
+            await app.error(m, e)
     else:
         return
