0bcc7f24be791b4e4abe61536d1ef63e50acefd0
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-11-13 08:39:56 +0700

    Update __main__.py

--- PunyaAlby/__main__.py
+++ PunyaAlby/__main__.py
@@ -13,7 +13,7 @@ from pyrogram import idle
 from uvloop import install
 
 from config import BOT_VER, CMD_HANDLER
-from PunyaAlby.userbot.client import app
+from PunyaAlby.userbot import app
 from PunyaAlby import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots
 from PunyaAlby.helpers.misc import create_botlog, git, heroku
 
